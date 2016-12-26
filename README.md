7ca5a131790e31d6df8501e1638dbe2f96138246
lorenmh <neutronstar@gmail.com>
2016-12-26 12:07:10 -0800

    removed bin

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ I copied this from the internet somewhere.
 The crucial thing is that I was able to compile this and get some triangles rendered. The goal is to restructure this program so I can understand how it works.
 
 [Code is originally from here](http://openme.gl/opengl-4-x-example-using-sdl2-and-glm/)
+
+# Steps
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 g++ -c LoadShaders.cpp
 #g++ ch03_drawcommands.cpp LoadShaders.o -lGL -lGLEW $(pkg-config glew --libs --cflags) $(sdl2-config --cflags) $(sdl2-config --libs) -o ch03_drawcommands
-g++ ch03_drawcommands.cpp LoadShaders.o -framework OpenGl -lGLEW $(sdl2-config --libs) -o ch03_drawcommands
+g++ ch03_drawcommands.cpp LoadShaders.o -framework OpenGl -lGLEW $(sdl2-config --libs) -o bin
 #g++ ch03_drawcommands.cpp LoadShaders.o -framework -lGL -lGLEW $(sdl2-config --libs) -o ch03_drawcommands
